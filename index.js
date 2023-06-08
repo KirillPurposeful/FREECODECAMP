ec6b64e289ae378cff9f328965fbd5d987bdf119
kirill.qa <kirill.qa@megapartners.io>
2023-06-09 00:25:05 +0200

    Appending Variables to Strings

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 //Constructing Strings with Variables
 const myName = "Kirill";
 const myStr = "My name is"+myName+"and I am well!";
+//Appending Variables to Strings
+const someAdjective = "qwe";
+let myStrTwo = "Learning to code is ";
+myStrTwo += someAdjective;
